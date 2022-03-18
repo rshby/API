1f50c6632da46b633bc2b1976df11b974713a3f5
Reo Sahobby <reoshby@gmail.com>
2022-03-18 11:14:32 +0700

    menambahkan data get api

---
+++ Client/wwwroot/js/getAjax.js
@@ -0,0 +1,42 @@
+
+// Get Ajax
+/*
+$.ajax({
+    type: "GET",
+    url: "https://swapi.dev/api/people",
+    data: {}
+}).done((result) => {
+    let data = result.results;
+    let text = "";
+    $.each(data, function (index, val) {
+        text += `<tr>
+                    <th scope="row">${index+1}</th>
+                    <td>${val.name}</td>
+                    <td>${val.height}</td>
+                    <td>${val.mass}</td>
+                    <td>${val.skin_color}</td>
+                    <td>${val.eye_color}</td>
+                    <td>${val.birth_year}</td>
+                </tr>`;
+    })
+    $('#tableSW').html(text);
+}).fail((err) => {
+    console.log(err);
+})
+*/
+
+// DataTable
+$(document).ready(function () {
+    $('#tableSW').DataTable({
+        "ajax": "https://swapi.dev/api/people",
+        "columns": [
+            { "results": "name"},
+            { "results": "name" },
+            { "results": "height" },
+            { "results": "mass" },
+            { "results": "skin_color" },
+            { "results": "eye_color" },
+            { "results": "birth_year" }
+        ]
+    });
+});
